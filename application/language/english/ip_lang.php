949321aaaadf7ff00add7421342e24aabaceecde
jon <jon@quadworks.co.uk>
2020-11-11 10:12:10 +0000

    Add new payment instructions field

--- application/language/english/ip_lang.php
+++ application/language/english/ip_lang.php
@@ -368,6 +368,7 @@ $lang = array(
     'payment_history'                              => 'Payment History',
     'payment_logs'                                 => 'Payment Logs',
     'payment_method'                               => 'Payment Method',
+    'payment_method_instructions'                  => 'Payment Instructions',
     'payment_method_already_exists'                => 'Payment Method already exists!',
     'payment_method_form'                          => 'Payment Method Form',
     'payment_methods'                              => 'Payment Methods',
